036c2099115506b873701e0b78ca18f40c71afd0
Morgan Grant <gramorgan@gmail.com>
2020-10-01 18:08:48 -0700

    refactor output system
    add 'output generators' which allow users to create arbitrary output formats.
    this allows market apps to have as many forms of output as are desired, and greatly simplifies the process of creating new output types.

--- views.py
+++ views.py
@@ -2,104 +2,34 @@ from otree.models import Session
 from otree.session import SESSION_CONFIGS_DICT
 from otree.common import get_models_module
 from django.template.response import TemplateResponse
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.urls import path
-from importlib import import_module
 import vanilla
-import csv
-import datetime
+from importlib import import_module
+
 from .models import Group as MarketGroup
-from .exchange.base import OrderStatusEnum
+from .output import DefaultJSONMarketOutputGenerator
 
-def make_json_export_path(session_config):
-    class MarketOutputJsonExportView(vanilla.View):
-
-        def get(self, request, *args, **kwargs):
-            session = get_object_or_404(Session, code=kwargs['session_code'])
-            group_data = []
-            for subsession in session.get_subsessions():
-                for group in subsession.get_groups():
-                    if not isinstance(group, MarketGroup):
-                        continue
-                    data = self.get_group_data(group)
-                    if data['exchange_data']:
-                        group_data.append(data)
-
-            response = JsonResponse(group_data, safe=False)
-            filename = '{} Market Data - session {} (accessed {}).json'.format(
-                session_config['display_name'],
-                kwargs['session_code'],
-                datetime.date.today().isoformat()
-            )
-            response['Content-Disposition'] = f'attachment; filename="{filename}"'
-            return response
-        
-        def order_to_output_dict(self, order):
-            return {
-                'time_entered': order.timestamp,
-                'price': order.price,
-                'volume': order.volume,
-                'is_bid': order.is_bid,
-                'pcode': order.pcode,
-                'traded_volume': order.traded_volume,
-                'id': order.id,
-                'status': OrderStatusEnum(order.status).name,
-                'time_inactive': order.time_inactive,
-            }
-        
-        def trade_to_output_dict(self, trade):
-            return {
-                'timestamp': trade.timestamp,
-                'taking_order_id': trade.taking_order.id,
-                'making_order_ids': [ o.id for o in trade.making_orders.all() ],
-            }
-        
-        def get_group_data(self, group: MarketGroup):
-            exchange_data = {}
-            exchange_query = group.exchanges.all().prefetch_related('orders', 'trades')
-            for exchange in exchange_query:
-                orders = [self.order_to_output_dict(e) for e in exchange.orders.all()]
-                trades = [self.trade_to_output_dict(e) for e in exchange.trades.all()]
-                exchange_data[exchange.asset_name] = {
-                    'orders': orders,
-                    'trades': trades,
-                }
-            return {
-                'round_number': group.round_number,
-                'id_in_subsession': group.id_in_subsession,
-                'exchange_data': exchange_data,
-            }
-    
-    app_name = session_config['name']
-    url_pattern = f'markets_export_json/{app_name}/<str:session_code>/'
-    url_name = f'markets_export_json_{app_name}'
-    return path(url_pattern, MarketOutputJsonExportView.as_view(), name=url_name)
-
-def make_csv_export_path(session_config, csv_gen_func):
-    class MarketOutputCsvExportView(vanilla.View):
+def make_export_path(config_name, output_generator_class):
+    class MarketOutputExportView(vanilla.View):
 
         def get(self, request, *args, **kwargs):
             session = get_object_or_404(Session, code=kwargs['session_code'])
+            output_generator = output_generator_class(session)
 
-            response = HttpResponse(content_type='text/csv')
-            filename = '{} Market Data (accessed {}).csv'.format(
-                session_config['display_name'],
-                datetime.date.today().isoformat()
-            )
+            response = HttpResponse(content_type=output_generator.get_mime_type())
+            filename = output_generator.get_filename()
             response['Content-Disposition'] = f'attachment; filename="{filename}"'
-
-            w = csv.writer(response)
-            w.writerows(csv_gen_func(session))
+            output_generator.write_output(response)
 
             return response
     
-    app_name = session_config['name']
-    url_pattern = f'markets_export_csv/{app_name}/<str:session_code>/'
-    url_name = f'markets_export_csv_{app_name}'
-    return path(url_pattern, MarketOutputCsvExportView.as_view(), name=url_name)
+    url_pattern = f'markets_export/{config_name}/{output_generator_class.__name__}/<str:session_code>/'
+    url_name = f'markets_export_{config_name}_{output_generator_class.__name__}'
+    return path(url_pattern, MarketOutputExportView.as_view(), name=url_name)
 
-def make_sessions_view(session_config, includes_csv_output):
+def make_sessions_view(session_config, output_generators):
     class MarketOutputSessionsView(vanilla.View):
         url_name = f'markets_sessions_{session_config["name"]}'
         url_pattern = f'^{url_name}/$'
@@ -123,9 +53,15 @@ def make_sessions_view(session_config, includes_csv_output):
             )
             sessions = Session.objects.filter(id__in=session_ids)
             context = {
-                'sessions': sessions,
                 'session_config': session_config,
-                'includes_csv_output': includes_csv_output,
+                'sessions': sessions,
+                'output_types': [
+                    {
+                        'url_name': f'markets_export_{session_config["name"]}_{generator_class.__name__}',
+                        'link_text': generator_class.download_link_text,
+                    }
+                    for generator_class in output_generators
+                ]
             }
             return TemplateResponse(request, 'otree_markets/MarketOutputSessionsView.html', context)
 
@@ -138,17 +74,18 @@ for session_config in SESSION_CONFIGS_DICT.values():
     if not any(issubclass(get_models_module(app_name).Group, MarketGroup) for app_name in session_config['app_sequence']):
         continue
 
-    # this isn't right, need to fix later
-    csv_gen_func = None
+    # output_generators is a list of subclasses of output.BaseMarketOutputGenerator
+    # if no output generators are provided by an app, just include the default json generator
+    output_generators = []
     for app_name in session_config['app_sequence']:
         try:
             output_module = import_module(f'{app_name}.output')
-            csv_gen_func = output_module.get_csv_output
+            output_generators.extend(output_module.output_generators)
         except (ImportError, AttributeError):
-            pass
+            continue
+    if not output_generators:
+        output_generators.append(DefaultJSONMarketOutputGenerator)
 
-    includes_csv_output = csv_gen_func is not None
-    markets_export_views.append(make_sessions_view(session_config, includes_csv_output))
-    markets_export_urls.append(make_json_export_path(session_config))
-    if includes_csv_output:
-        markets_export_urls.append(make_csv_export_path(session_config, csv_gen_func))
+    markets_export_views.append(make_sessions_view(session_config, output_generators))
+    for output_generator in output_generators:
+        markets_export_urls.append(make_export_path(session_config['name'], output_generator))
